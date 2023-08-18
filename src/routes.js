efd30b5eb01c4e2c20a88c3aa93228b2304ab9c4
Stanley-Rodrigues <stanleyrodriguessilva@gmail.com>
2023-08-18 14:41:49 -0300

    Up project api

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+
+const routes = new Router()
+routes.get('/', (request, response) => {
+  return response.json({ message: 'hello world!' })
+})
+
+export default routes
