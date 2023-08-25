eba6c66653659b7886db703715de838f0fe0a19c
Stanley-Rodrigues <stanleyrodriguessilva@gmail.com>
2023-08-25 19:52:07 -0300

    add config models

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+import User from './app/models/User'
 
 const routes = new Router()
-routes.get('/', (request, response) => {
-  return response.json({ message: 'hello world!' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'stanley',
+    email: 'stanley.12@gmail.com',
+    password_hash: '123abc',
+  })
+  return response.json(user)
 })
 
 export default routes
