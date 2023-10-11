a26864fcae8e874b52aaf038e9b4de3bd6457df3
Stanley-Rodrigues <stanleyrodriguessilva@gmail.com>
2023-10-11 13:20:49 -0300

    add ypu validate

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,7 @@
 import { Router } from 'express'
-import { v4 } from 'uuid'
-import User from './app/models/User'
+import UserController from './app/controllers/UserController'
 
 const routes = new Router()
-routes.get('/', async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: 'stanley',
-    email: 'stanley.12@gmail.com',
-    password_hash: '123abc',
-  })
-  return response.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
