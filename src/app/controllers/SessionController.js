3919f0fe37fb0601899df3d2d1bb2f6c3bb4f19c
Stanley-Rodrigues <stanleyrodriguessilva@gmail.com>
2023-10-20 16:05:15 -0300

    Ajustes feitos no session controller

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -8,23 +8,32 @@ class SessionController {
       password: Yup.string().required(),
     })
 
-    const userEmailOrPasswordIncorrect = () => {
-      return res
-        .status(401)
-        .json({ error: 'make sure your password or email are correct' })
-    }
+    // const userEmailOrPasswordIncorrect = () => {
+    //   return res
+    //     .status(401)
+    //     .json({ error: 'make sure your password or email are correct' })
+    // }
 
     const { email, password } = req.body
 
-    if (!(await schema.isValid(req.body))) userEmailOrPasswordIncorrect()
+    if (!(await schema.isValid(req.body)))
+      return res
+        .status(401)
+        .json({ error: 'make sure your password or email are correct' })
 
     const user = await User.findOne({
       where: { email },
     })
 
-    if (!user) userEmailOrPasswordIncorrect()
+    if (!user)
+      return res
+        .status(401)
+        .json({ error: 'make sure your password or email are correct' })
 
-    if (!(await user.checkPassword(password))) userEmailOrPasswordIncorrect()
+    if (!(await user.checkPassword(password)))
+      return res
+        .status(401)
+        .json({ error: 'make sure your password or email are correct' })
 
     return res.json({
       id: user.id,
