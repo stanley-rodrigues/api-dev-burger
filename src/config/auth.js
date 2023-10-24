57a94b96d14e4a372970874c29b4dba8d9a4afc6
Stanley-Rodrigues <stanleyrodriguessilva@gmail.com>
2023-10-24 15:41:03 -0300

    add configuration jwt token and decoded with jwt verify

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'c7656ce3fbb462c82bad4e11fc7f4165',
+  expiresIn: '5d',
+}
--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,15 @@ import multerConfig from './config/multer'
 import UserController from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 import ProductController from './app/controllers/ProductController'
+import authMiddleware from './app/middlewares/auth'
 
 const upload = multer(multerConfig)
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+
+routes.use(authMiddleware)
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
 
